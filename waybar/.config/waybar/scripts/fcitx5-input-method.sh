8431249395f2a27e040649f3144b1b730e12a04b
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-06-05 02:25:25 +0900

    新規: 現在の入力メソッドを取得し表示するスクリプトを追加

---
+++ waybar/.config/waybar/scripts/fcitx5-input-method.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 現在の入力メソッドを取得（例：mozc, anthy, etc）
+current_im=$(fcitx5-remote -n)
+
+# シンプルに表示したい名前に変換（必要に応じて）
+case "$current_im" in
+  "mozc")
+    mode="  あ"
+    ;;
+  "keyboard-us")
+    mode="  Ａ"
+    ;;
+  *)
+    first_char="${current_im:0:1}"
+    first_char_upper=$(echo "$first_char" | tr '[:lower:]' '[:upper:]')
+    ord=$(printf "%d" "'$first_char_upper")
+    fullwidth_code=$((0xFF00 + ord - 0x20))
+    fullwidth_char=$(printf "\\U%08x" "$fullwidth_code")
+    mode="  $fullwidth_char"
+    ;;
+esac
+
+echo "{\"text\": \"$mode\", \"tooltip\": \"Input Method: $current_im\"}"
