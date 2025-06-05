6add4841bf0127d79f3a250e6a87fcd313b11304
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-06-05 10:51:32 +0900

    修正: waypaperの設定を更新し、壁紙パスと表示オプションを変更 新規: 壁紙を設定するスクリプトを追加

---
+++ waypaper/.config/waypaper/load_walpy.sh
@@ -0,0 +1,9 @@
+WALLPAPER=$(grep "wallpaper =" ~/.config/waypaper/config.ini | cut -d '=' -f2- | xargs)
+# チルダ展開
+WALLPAPER="${WALLPAPER/#\~/$HOME}"
+
+if [[ -f "$WALLPAPER" ]]; then
+  wal -i "$WALLPAPER"
+else
+  echo "[!] Invalid wallpaper path: $WALLPAPER"
+fi
